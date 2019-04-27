f580f5fbb579fb33ef1666b77275b70e603e48b7
mrbandler <baudler.michael@gmail.com>
2019-04-27 12:08:59 +0200

    init: UE4 project created

---
+++ Source/BloodAndSoul/Public/GameModes/BloodAndSoulGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BloodAndSoulGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOODANDSOUL_API ABloodAndSoulGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
